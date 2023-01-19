303fbe8f4b7df82bd0608cdb2efa03a81c0b9dd4
Dheeraj Sharma <at60sharwan@gmail.com>
2023-01-19 17:00:25 +0530

    Created Work section Third row.

--- src/Work/Work.jsx
+++ src/Work/Work.jsx
@@ -30,6 +30,7 @@ const Work = () => {
           />
         </div>
       </div>
+
       <div className='work__row'>
         <div className='work__leftContainer'>
           <img
@@ -60,6 +61,59 @@ const Work = () => {
               subtext='Follow the instructions to setup and customize the template to your needs.'
             />
           </div>
+        </div>
+      </div>
+
+      <div className='work__row'>
+        <div className='work__leftContainer'>
+          <p className=' purple work__text'>VALUES</p>
+          <h2 className='work__heading'>
+            We Always Abide by Our <span className='purple'>Principles.</span>
+          </h2>
+          <p className='work__subtext'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <div className='work__cards'>
+            <div className='left__card'>
+              <div className='left__card--div'>
+                <img
+                  src='./src/assets/fast-icon.svg'
+                  alt=''
+                  className='card--img'
+                />
+                <p className='title'>Affordable</p>
+              </div>
+              <p className='subtext subtext__ext'>
+                We promise to offer you the best rate we can - at par with the
+                industry standard.
+              </p>
+            </div>
+            <div className='right__card'>
+              <div className='right__card--div'>
+                <img
+                  src='./src/assets/fast-icon.svg'
+                  alt=''
+                  className='card--img'
+                />
+                <p className='title'>Professionalism</p>
+              </div>
+              <p className='subtext subtext__ext'>
+                We assure you that our templates are designed and created by
+                professional designers.
+              </p>
+            </div>
+          </div>
+          <button className='work__btn'>Learn More</button>
+        </div>
+        <div className='work__rightContainer'>
+          <img
+            className='work__img3'
+            src='./src/assets/value-section.svg'
+            alt=''
+          />
         </div>
       </div>
     </section>
